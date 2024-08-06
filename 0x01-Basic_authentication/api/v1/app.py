c8dc4ff305ede94a8b80959d16189fe3e19c1b1f
Olutunmibi Iseoluwa <olatundeolutunmibi@gmail.com>
2024-08-06 22:59:08 +0000

    Task Passed

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python3
+"""Route module for the API.
 """
-Route module for the API
-"""
+import os
 from os import getenv
-from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-import os
-
 
 from api.v1.views import app_views
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -23,11 +21,13 @@ if auth_type == 'auth':
 if auth_type == 'basic_auth':
     auth = BasicAuth()
 
+
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler
+    """Not found handler.
     """
     return jsonify({"error": "Not found"}), 404
+
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
@@ -60,6 +60,7 @@ def authenticate_user():
                 abort(401)
             if user is None:
                 abort(403)
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
